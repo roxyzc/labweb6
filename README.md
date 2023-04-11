ce944300f6a91466911b4eac1556b949837be0eb
roxyzc <roxyzc12@gmail.com>
2023-04-11 19:19:09 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# lab5
+
+**_<p>Nama : Ridho Pikriyansyah</p>_**
+**_<p>Nim : 312110169</p>_**
+**_<p>Kelas: TI.21.A.3</p>_**
+
+## Home
+
+![roxyzc](./img/home.png)
+
+## Add
+
+![roxyzc](./img/add.png)
+
+## Update
+
+![roxyzc](./img/update.png)
+
+## About
+
+![roxyzc](./img/about.png)
+
+## Contact
+
+![roxyzc](./img/contact.png)
---
+++ module/artikel/contact.php
@@ -0,0 +1,7 @@
+<div class="main">
+    <h2>Contact Me</h2>
+    <div class="contact">
+        <a target="_blank" class="instagram" href="https://www.instagram.com/ridh15_/?next=%2F"><i class="fa-brands fa-instagram"></i></a>
+        <a target="_blank" class="github" href="https://github.com/roxyzc"><i class="fa-brands fa-github"></i></a>
+    </div>
+</div>
